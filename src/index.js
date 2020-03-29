90be6b9ccd5b3d556471deae25aa233220f13a06
Sean Coley <seanwcoley@gmail.com>
2020-03-29 15:09:59 +0200

    Add apollo and init query

--- src/index.js
+++ src/index.js
@@ -1,9 +1,54 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+
 import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+
+const client = new ApolloClient({
+  uri: "https://swapi.graph.cool/"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        Person(id: "cj0nv9pa9ewco0130v4ocnhed") {
+          id
+          name
+          hairColor
+          eyeColor
+          gender
+          mass
+          height
+          homeworld {
+            id
+            name
+          }
+          films {
+            id
+            title
+          }
+          species {
+            id
+            name
+          }
+          starships {
+            id
+            name
+          }
+          vehicles {
+            id
+            name
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(
   <BrowserRouter>
