27cf973232584aad268b98f50153d5fc98152953
Sean Coley <seanwcoley@gmail.com>
2020-04-05 17:53:01 +0200

    Convert TopNode component to function using gql useQuery

--- src/components/TopNode.js
+++ src/components/TopNode.js
@@ -1,92 +1,24 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@apollo/react-hooks";
 
-import SubNode from "./SubNode";
+import queries from "../queries";
+import helpers from "./helpers";
 
-import "../styles/TopNode.css";
-import schemaJSON from "../schema.json";
-import fn from "./helpers";
+function TopNode({ match }) {
+  const { dimension, id } = match.params;
+  const [queryData, setQueryData] = useState({});
+  let { loading, error, data } = useQuery(queries[dimension], {
+    variables: { id }
+  });
 
-class TopNode extends Component {
-  static defaultProps = {
-    seedURL: "https://swapi.co/api/people/4"
-  };
-  state = {
-    loading: true,
-    data: {}
-  };
-  async componentDidMount() {
-    const response = await axios.get(this.props.seedURL);
-    // format incoming data according to shape in schema.json
-    const formattedData = fn.formatData(response.data, "people");
-    const connectionKeysArray = Object.keys(formattedData.connections);
-    const promises = Promise.all(
-      connectionKeysArray.map(key => {
-        const { urlArray } = formattedData.connections[key];
-        // coerce to array where only 1 URL given as string
-        if (typeof urlArray === "string")
-          return Promise.all([axios.get(urlArray)]);
-        return Promise.all(urlArray.map(url => axios.get(url)));
-      })
-    );
-    promises.then(groupPromiseResult => {
-      console.log(groupPromiseResult);
-    });
-    this.setState({ data: formattedData, loading: false }, () => {});
-  }
-  generateListItems = (schema, data) => {
-    return schema.enumerablePropertiesArray.map(prop => {
-      const { enumerablePropertyDisplayName, enumerablePropertyName } = prop;
-      return (
-        <li key={enumerablePropertyName}>{`${enumerablePropertyDisplayName}: ${
-          data[schema.enumerablePropertyDimensionName][enumerablePropertyName]
-        }`}</li>
-      );
-    });
-  };
-  generateSubNodes = (connectionNames, connectionData) => {
-    return connectionNames.map(connectionName => {
-      const { displayName, urlArray } = connectionData[connectionName];
-      return (
-        <SubNode
-          key={connectionName}
-          dim={connectionName}
-          displayName={displayName}
-          urlArray={urlArray}
-        />
-      );
-    });
-  };
-  render() {
-    const { data, loading } = this.state;
-    const connectionKeys = !loading ? Object.keys(data.connections) : null;
-    // const { dimension } = this.props.match.params;
-    // console.log(dimension);
-    const dimension = "people";
-    const schema = schemaJSON[dimension].data;
-    return (
-      <div className="TopNode">
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <div>
-            <h1>{data.name}</h1>
-            {schema.dimensionEnumerablePropertiesArray.map(eProp => {
-              return (
-                <div key={eProp.enumerablePropertyDisplayName}>
-                  <h3>{eProp.enumerablePropertyDisplayName}</h3>
-                  <ul className="TopNode-list">
-                    {this.generateListItems(eProp, data)}
-                  </ul>
-                </div>
-              );
-            })}
-            <div>{this.generateSubNodes(connectionKeys, data.connections)}</div>
-          </div>
-        )}
-      </div>
-    );
-  }
+  useEffect(() => {
+    if (!loading) setQueryData(data[helpers.capitaliseEachWord(dimension)]);
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return <div>{queryData.name}</div>;
 }
 
 export default TopNode;
