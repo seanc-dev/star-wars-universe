a413a10a99a906aa251af549bd01f36030370fba
Sean Coley <seanwcoley@gmail.com>
2020-04-06 01:50:55 +0200

    Update queries

--- src/queries.js
+++ src/queries.js
@@ -1,6 +1,34 @@
 import { gql } from "apollo-boost";
 
 const queries = {
+  nodeList: gql`
+    query {
+      allPersons {
+        id
+        name
+      }
+      allFilms {
+        id
+        title
+      }
+      allPlanets {
+        id
+        name
+      }
+      allSpecies {
+        id
+        name
+      }
+      allStarships {
+        id
+        name
+      }
+      allVehicles {
+        id
+        name
+      }
+    }
+  `,
   person: gql`
     query Person($id: ID!) {
       Person(id: $id) {
@@ -67,34 +95,85 @@ const queries = {
         }
       }
     }
+  `,
+  planet: gql`
+    query Planet($id: ID!) {
+      Planet(id: $id) {
+        id
+        name
+        population
+        climate
+        terrain
+        surfaceWater
+        gravity
+        diameter
+        orbitalPeriod
+        rotationPeriod
+        films {
+          id
+          name
+        }
+        residents {
+          id
+          name
+        }
+      }
+    }
+  `,
+  species: gql`
+    query Species($id: ID!) {
+      Species(id: $id) {
+        averageHeight
+        averageLifespan
+        classification
+        designation
+        eyeColor
+        films {
+          id
+          title
+        }
+        hairColor
+        id
+        language
+        name
+        people {
+          id
+          name
+        }
+        skinColor
+      }
+    }
+  `,
+  startship: gql`
+    query Starship($id: ID!) {
+      Startship(id: $id) {
+        cargoCapacity
+        class
+        consumables
+        costInCredits
+        crew
+        films {
+          id
+          title
+        }
+        hyperdriveRating
+        id
+        length
+        manufacturer
+        maxAtmospheringSpeed
+        mglt
+        name
+        passengers {
+          id
+          name
+        }
+        pilots {
+          id
+          name
+        }
+      }
+    }
   `
-  // planet: gql`{
-  //   Planet ($id: ID!) {
-  //     id
-  //     name
-  //     population
-  //     climate
-  //     terrain
-  //     surfaceWater
-  //     gravity
-  //     diameter
-  //     orbitalPeriod
-  //     rotationPeriod
-  //     films {
-  //       id
-  //       name
-  //     }
-  //     residents {
-  //       id
-  //       name
-  //     }
-  //   }}`
-  // getSpeciesById: (id) => {
-  //   gql`
-  //   Species (id: ${id} {
-
-  //   })`
-  // }
 };
 
 export default queries;
