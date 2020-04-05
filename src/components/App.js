35174a7654fca40e8a7fbf96ccac89b6f2c92686
Sean Coley <seanwcoley@gmail.com>
2020-04-06 01:51:42 +0200

    Move nodelist data management into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import TopNode from "./TopNode";
+import NodeList from "./NodeList";
+import Node from "./Node";
 import NotFound from "./NotFound";
+import { useQuery } from "@apollo/react-hooks";
+
+import queries from "../queries";
 
 import "../styles/App.css";
 
 function App() {
+  const { loading, error, data } = useQuery(queries.nodeList);
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={TopNode} />
-        <Route exact path="/:dimension/:id" component={TopNode} />
-        <Route path="" component={NotFound} />
-      </Switch>
+      {loading ? (
+        <p>Loading..</p>
+      ) : (
+        <div>
+          <NodeList data={data} />
+          <Switch>
+            <Route exact path="/" component={Node} />
+            <Route exact path="/:dimension/:id" component={Node} />
+            <Route path="" component={NotFound} />
+          </Switch>
+        </div>
+      )}
     </div>
   );
 }
