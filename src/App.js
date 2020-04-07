bab43194cfbf7bc29b4264e079a91be50e0472bf
Sean Coley <seanwcoley@gmail.com>
2020-04-07 11:17:29 +0200

    Remove header from App component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Routes from "./scenes/Routes/Routes.scene";
 function App() {
   return (
     <div className="App">
-      <h1>Explore the Star Wars Universe</h1>
       <Routes />
     </div>
   );
