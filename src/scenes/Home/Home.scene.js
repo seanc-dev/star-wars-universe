d834a5efa1e78e52f48b3e33f664e251b8984295
Sean Coley <seanwcoley@gmail.com>
2020-04-07 11:18:14 +0200

    Add header to Home scene

--- src/scenes/Home/Home.scene.js
+++ src/scenes/Home/Home.scene.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
+      <h1>Explore the Star Wars Universe</h1>
       <h2>
         This app provides an interactive walkthrough of the Star Wars Universe.
       </h2>
